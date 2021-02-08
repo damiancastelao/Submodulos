daa09ab615e115ef014ee93f4500d99fd9e0de3d
Damian Nogueiras <damiannogueiras@gmail.com>
2021-02-08 21:05:58 +0100

    utilizacion de libreria

--- src/com/cod/Main.java
+++ src/com/cod/Main.java
@@ -1,8 +1,11 @@
 package com.cod;
 
+import paquetemismetodos.MisMetodos;
+
 public class Main {
 
     public static void main(String[] args) {
 	// Proyecto principal
+        paquetemismetodos.MisMetodos.hello();
     }
 }
